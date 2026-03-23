68e91461fe8d21941da3ff2e7f7c75f891b9d804
Michał Cychner <254737@edu.p.lodz.pl>
2026-03-23 20:56:49 +0100

    Presentation Model layer

---
+++ PresentationModel/ModelAbstractApi.cs
@@ -0,0 +1,20 @@
+﻿using Data;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace PresentationModel
+{
+    public abstract class ModelAbstractApi
+    {
+        public abstract ObservableCollection<IBall> Balls { get; }
+        public abstract void Start(int ballsCount);
+        public abstract void Stop();
+
+        public static ModelAbstractApi CreateApi()
+        {
+            return new ModelApi();
+        }
+    }
+}
---
+++ PresentationModel/ModelApi.cs
@@ -0,0 +1,36 @@
+﻿using BusinessLogic;
+using Data;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace PresentationModel
+{
+    internal class ModelApi : ModelAbstractApi
+    {
+        private readonly LogicAbstractApi _logicApi;
+        public override ObservableCollection<IBall> Balls { get; } = new ObservableCollection<IBall>();
+
+        public ModelApi()
+        {
+            _logicApi = LogicAbstractApi.CreateApi();
+        }
+
+        public override void Start(int ballsCount)
+        {
+            _logicApi.CreateBalls(ballsCount, 600, 400); // 600x400 to przykładowy rozmiar stołu
+            Balls.Clear();
+            foreach (var ball in _logicApi.GetBalls())
+            {
+                Balls.Add(ball);
+            }
+            _logicApi.StartSimulation();
+        }
+
+        public override void Stop()
+        {
+            _logicApi.StopSimulation();
+        }
+    }
+}
