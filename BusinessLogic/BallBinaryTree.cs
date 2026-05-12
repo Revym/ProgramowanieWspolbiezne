dceebd847efcbcb23108c219f43d6b2bbbe5d15a
BlueMoon903 <cezarykropski.bm@gmail.com>
2026-05-12 12:50:03 +0200

    Collisions

---
+++ BusinessLogic/BallBinaryTree.cs
@@ -0,0 +1,80 @@
+﻿using Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLogic
+{
+    internal class TreeNode
+    {
+        public IBall Ball { get; }
+        public TreeNode? Left { get; set; }
+        public TreeNode? Right { get; set; }
+
+        public TreeNode(IBall ball)
+        {
+            Ball = ball;
+        }
+    }
+
+    internal class BallBinaryTree
+    {
+        private TreeNode? _root;
+
+        public void Build(IEnumerable<IBall> balls)
+        {
+            _root = BuildRecursive(balls.ToList(), 0);
+        }
+
+        private TreeNode? BuildRecursive(List<IBall> balls, int depth)
+        {
+            if (balls.Count == 0) return null;
+
+            int axis = depth % 2;
+
+            balls.Sort((a, b) => axis == 0 ? a.X.CompareTo(b.X) : a.Y.CompareTo(b.Y));
+
+            int medianIndex = balls.Count / 2;
+            TreeNode node = new TreeNode(balls[medianIndex]);
+
+            node.Left = BuildRecursive(balls.GetRange(0, medianIndex), depth + 1);
+            node.Right = BuildRecursive(balls.GetRange(medianIndex + 1, balls.Count - (medianIndex + 1)), depth + 1);
+
+            return node;
+        }
+
+        public List<IBall> FindPotentialCollisions(IBall target, double searchRadius)
+        {
+            List<IBall> result = new List<IBall>();
+            SearchRecursive(_root, target, searchRadius, 0, result);
+            return result;
+        }
+
+        private void SearchRecursive(TreeNode? node, IBall target, double searchRadius, int depth, List<IBall> result)
+        {
+            if (node == null) return;
+
+            if (node.Ball != target)
+            {
+                double dist = Math.Sqrt(Math.Pow(node.Ball.X - target.X, 2) + Math.Pow(node.Ball.Y - target.Y, 2));
+                if (dist <= searchRadius)
+                {
+                    result.Add(node.Ball);
+                }
+            }
+
+            int axis = depth % 2;
+            double targetCoord = axis == 0 ? target.X : target.Y;
+            double nodeCoord = axis == 0 ? node.Ball.X : node.Ball.Y;
+
+            if (targetCoord - searchRadius <= nodeCoord)
+            {
+                SearchRecursive(node.Left, target, searchRadius, depth + 1, result);
+            }
+            if (targetCoord + searchRadius >= nodeCoord)
+            {
+                SearchRecursive(node.Right, target, searchRadius, depth + 1, result);
+            }
+        }
+    }
+}
--- BusinessLogic/LogicApi.cs
+++ BusinessLogic/LogicApi.cs
@@ -1,6 +1,7 @@
 ﻿using Data;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Timers;
 
@@ -9,8 +10,9 @@ namespace BusinessLogic
     internal class LogicApi : LogicAbstractApi
     {
         private readonly DataAbstractApi _dataApi;
-        private System.Timers.Timer? _timer;
-        private readonly Random _random = new Random();
+        private readonly object _collisionLock = new object();
+
+        private readonly BallBinaryTree _kdTree = new BallBinaryTree();
 
         public override int BoardWidth => _dataApi.BoardWidth;
         public override int BoardHeight => _dataApi.BoardHeight;
@@ -25,6 +27,11 @@ namespace BusinessLogic
         public override void CreateBalls(int count, int boardWidth, int boardHeight)
         {
             _dataApi.CreateBalls(count, boardWidth, boardHeight);
+
+            foreach (var ball in _dataApi.GetBalls())
+            {
+                ball.PropertyChanged += OnBallMoved;
+            }
         }
 
         public override IEnumerable<IBallStatus> GetBallsStatus()
@@ -34,72 +41,104 @@ namespace BusinessLogic
 
         public override void StartSimulation()
         {
-            if (_timer != null) return;
-
-            _timer = new System.Timers.Timer(16);
-
-            _timer.Elapsed += MoveBalls;
-
-            _timer.AutoReset = true;
-            _timer.Start();
+            SimulationUpdated?.Invoke(this, GetBallsStatus().ToList());
         }
 
         public override void StopSimulation()
         {
-            if (_timer != null)
+            foreach (var ball in _dataApi.GetBalls())
             {
-                _timer.Stop();
-                _timer.Dispose();
-                _timer = null;
+                ball.PropertyChanged -= OnBallMoved;
             }
         }
 
-        private void MoveBalls(object? sender, ElapsedEventArgs e)
+        private void OnBallMoved(object? sender, PropertyChangedEventArgs e)
         {
-            int width = _dataApi.BoardWidth;
-            int height = _dataApi.BoardHeight;
-
-            lock (_dataApi)
+            if (sender is IBall ball && (e.PropertyName == "X" || e.PropertyName == "Y"))
             {
-                foreach (var ball in _dataApi.GetBalls())
+                CheckCollisions(ball);
+                SimulationUpdated?.Invoke(this, GetBallsStatus().ToList());
+            }
+        }
+
+        private void CheckCollisions(IBall currentBall)
+        {
+            lock (_collisionLock)
+            {
+                var allBalls = _dataApi.GetBalls().ToList();
+
+                HandleWallCollisions(currentBall);
+
+                _kdTree.Build(allBalls);
+
+                double searchArea = currentBall.Radius + 25.0;
+
+                var nearbyBalls = _kdTree.FindPotentialCollisions(currentBall, searchArea);
+
+                foreach (var otherBall in nearbyBalls)
                 {
-                    double newX = ball.X + ball.Velocity.X;
-                    double newY = ball.Y + ball.Velocity.Y;
+                    double xDiff = currentBall.X - otherBall.X;
+                    double yDiff = currentBall.Y - otherBall.Y;
+                    double distanceSquared = xDiff * xDiff + yDiff * yDiff;
+                    double distance = Math.Sqrt(distanceSquared);
 
-                    double currentVx = ball.Velocity.X;
-                    double currentVy = ball.Velocity.Y;
+                    double minDistance = currentBall.Radius + otherBall.Radius;
 
-                    if (newX <= ball.Radius)
+                    if (distance <= minDistance)
                     {
-                        newX = ball.Radius;
-                        currentVx = -currentVx;
-                    }
-                    else if (newX >= width - ball.Radius)
-                    {
-                        newX = width - ball.Radius;
-                        currentVx = -currentVx;
-                    }
+                        double vxDiff = currentBall.Velocity.X - otherBall.Velocity.X;
+                        double vyDiff = currentBall.Velocity.Y - otherBall.Velocity.Y;
+                        double dotProduct = vxDiff * xDiff + vyDiff * yDiff;
 
-                    if (newY <= ball.Radius)
-                    {
-                        newY = ball.Radius;
-                        currentVy = -currentVy;
-                    }
-                    else if (newY >= height - ball.Radius)
-                    {
-                        newY = height - ball.Radius;
-                        currentVy = -currentVy;
-                    }
+                        if (dotProduct < 0)
+                        {
+                            double m1 = currentBall.Mass;
+                            double m2 = otherBall.Mass;
 
-                    ball.X = newX;
-                    ball.Y = newY;
-                    ball.Velocity = new Vector2D(currentVx, currentVy);
+                            double scalar1 = (2 * m2 / (m1 + m2)) * (dotProduct / distanceSquared);
+                            double scalar2 = (2 * m1 / (m1 + m2)) * (dotProduct / distanceSquared);
+
+                            double newVx1 = currentBall.Velocity.X - scalar1 * xDiff;
+                            double newVy1 = currentBall.Velocity.Y - scalar1 * yDiff;
+
+                            double newVx2 = otherBall.Velocity.X - scalar2 * (-xDiff);
+                            double newVy2 = otherBall.Velocity.Y - scalar2 * (-yDiff);
+
+
+                            currentBall.Velocity = new Vector2D(newVx1, newVy1);
+                            otherBall.Velocity = new Vector2D(newVx2, newVy2);
+                        }
+                    }
                 }
             }
+        }
 
-            var snapshot = GetBallsStatus().ToList();
+        private void HandleWallCollisions(IBall currentBall)
+        {
+            int width = BoardWidth;
+            int height = BoardHeight;
 
-            SimulationUpdated?.Invoke(this, snapshot);
+            if (currentBall.X <= currentBall.Radius)
+            {
+                currentBall.X = currentBall.Radius;
+                currentBall.Velocity = new Vector2D(-currentBall.Velocity.X, currentBall.Velocity.Y);
+            }
+            else if (currentBall.X >= width - currentBall.Radius)
+            {
+                currentBall.X = width - currentBall.Radius;
+                currentBall.Velocity = new Vector2D(-currentBall.Velocity.X, currentBall.Velocity.Y);
+            }
+
+            if (currentBall.Y <= currentBall.Radius)
+            {
+                currentBall.Y = currentBall.Radius;
+                currentBall.Velocity = new Vector2D(currentBall.Velocity.X, -currentBall.Velocity.Y);
+            }
+            else if (currentBall.Y >= height - currentBall.Radius)
+            {
+                currentBall.Y = height - currentBall.Radius;
+                currentBall.Velocity = new Vector2D(currentBall.Velocity.X, -currentBall.Velocity.Y);
+            }
         }
     }
 }
